6ec31d98aea2e4fc32d55b6a97620c469a60ab26
AROVEN TECH <iammshyam@gmail.com>
2026-07-26 20:38:04 +0530

    SEO Update: Align sitemap.js and robots.js domain with https://aroventech.site

--- app/robots.js
+++ app/robots.js
@@ -4,6 +4,6 @@ export default function robots() {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://www.aroventech.site/sitemap.xml',
+    sitemap: 'https://aroventech.site/sitemap.xml',
   };
 }
--- app/sitemap.js
+++ app/sitemap.js
@@ -1,5 +1,5 @@
 export default function sitemap() {
-  const baseUrl = 'https://www.aroventech.site';
+  const baseUrl = 'https://aroventech.site';
 
   return [
     {
